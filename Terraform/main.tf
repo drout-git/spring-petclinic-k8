73c397b579adf9a3a2b9c8d993c73279454d002d
drout-git <drout4707@gmail.com>
2025-06-24 01:32:58 +0530

    Update main.tf
    Signed-off-by: drout-git <drout4707@gmail.com>

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,6 +4,33 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = ">= 5.40.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
       version = ">= 5.0.0"
     }
   }
